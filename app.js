3e48abb3036159d9747a01ece7fa82aca4c50af7
Louis <fkyustd0478@gmail.com>
2026-04-05 21:27:23 +0800

    修正app.js路徑問題

--- app.js
+++ app.js
@@ -666,7 +666,7 @@ try {
     }).mount('#app');
 
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js');
+        navigator.serviceWorker.register('./sw.js');
     }
 } catch (e) { 
     const errorReporter = document.getElementById('error-reporter');
